287a3d969fc003b51170ce81473e0b25bb30794e
erika-arriaga <juurivera666@gmail.com>
2022-04-04 23:18:32 -0400

    first commit

---
+++ principal.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+char tecla;
+int main()
+
+{
+	
+
+
+	do
+	{
+		cout << "Mi nombre es Erika Arriaga Ortiz \n";
+		cout << "estudio ing sistemas \n";
+		cout << "mi documento de identidad Ci:13091309 \n";
+		cout << "me gusta jugar dota \n";
+		cout << "trabajo 4 dias a la semana \n";
+		cout << "Soy de Potosi pero radicamos aqui hace 20 años \n";
+		cout << "me gusta comer de todo \n";
+		cout << "vivo con mis padres \n";
+		cout << "amo a los animales \n";
+		cout << "tengo una coneja que es adicta a las croquetas de perro \n";
+		cout << "tengo 2 perros, 1 ratita de laboratorio y 1 coneja \n";
+		cout << "vivo en una zona  muy peligrosa (roja) de la ciudad \n";
+		cout << "vivo a lado de la felcc \n \n \n \n\n  ";
+		cout << "Para repetir tecla (s)\n  Para cerrar tecla(n)\n";
+		cin >> tecla;
+	
+		
+
+
+	} while (tecla != 'n');
+
+
+	return 0;
+}
